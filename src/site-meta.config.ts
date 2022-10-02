7b5fde123a6d4a929c8a8615607fe928b7b1bfe6
Bytewaiser <bytewaiser@gmail.com>
2022-10-03 00:36:05 +0300

    End of index page I hope

--- src/site-meta.config.ts
+++ src/site-meta.config.ts
@@ -1,9 +1,9 @@
 const siteMeta = {
-  title: "Astro Theme Cactus",
-  description: "An opinionated starter theme for Astro",
+  title: "Hasan H. Yurdagül",
+  description: "Hasan Hüseyin Yurdagül's website",
   lang: "en-GB",
   ogLocale: "en_GB",
-  githubUrl: "https://github.com/chrismwilliams/astro-theme-cactus",
+  githubUrl: "https://github.com/bytewaiser",
   themeColorLight: "#fafafa",
   themeColorDark: "#1d1f21",
 }
